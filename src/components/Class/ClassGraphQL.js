aa55bb1c7d1fec8e64daf6757f200278f015a1f4
stectr <microtechsteve@gmail.com>
2022-02-27 20:35:58 -0800

    done?

--- src/components/Class/ClassGraphQL.js
+++ src/components/Class/ClassGraphQL.js
@@ -2,15 +2,31 @@ import "./Class.css";
 import React, {useState, useEffect} from 'react';
 
 
-function Class(props) {
+function ClassGraphQL(props) {
 
     const [classInfo, setClassInfo] = useState({});
 
-    const url = "https://api.peterportal.org/rest/v0/courses/"
+    const url = "https://api.peterportal.org/graphql"
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = await fetch(url + props.name);
+            const query = `
+                query {
+                    course(id:"${props.name}") {
+                        title
+                        department
+                        description
+                    }
+                }
+            `
+
+            const response = await fetch(url, {
+                method: "POST",
+                body: JSON.strify({query}) ,
+                headers: {
+                    "Content-type": "application/json"
+                }
+            });
             const data = await response.json();
             console.log(data)
             setClassInfo(data);
@@ -19,13 +35,13 @@ function Class(props) {
     }, [props.name]);
 
     let info;
-    if(classInfo.id) {
+    if(classInfo) {
         info = <div className="information">
             <p id="title"> {classInfo.title} </p>
             <p id="department"> {classInfo.department} </p>
             <p id="description"> {classInfo.description} </p>
         </div>
-    } else if (classInfo.error) {
+    } else if (classInfo == null) {
         info = <p>Class Not Found</p>
     }
     else {
@@ -46,4 +62,4 @@ function Class(props) {
     )
 }
 
-export default Class;
+export default ClassGraphQL;
